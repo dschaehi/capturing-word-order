bbcad1a041a4705d1dd74097a2c3b90220f48302
Jae Hee Lee <lee@informatik.uni-hamburg.de>
2020-07-11 13:25:31 +0200

    Adding hyperparameters

--- src/tune_bigram_models.py
+++ src/tune_bigram_models.py
@@ -29,13 +29,14 @@ if __name__ == "__main__":
     if not args.smoke_test and repo.is_dirty():
         raise RepositoryDirtyError(repo, "Have you forgotten to commit the changes?")
 
-    corpus_size = 1000000
+    corpus_size = 100
+
     config = {
         # A trick to log the SHA of the git HEAD.
         "SHA": tune.grid_search([sha]),
         "corpus_size": tune.grid_search([corpus_size]),
-        "margin": tune.grid_search([0.2]),
-        "lr": tune.grid_search([0.1]),
+        "margin": tune.loguniform(0.0001, 0.2),
+        "lr": tune.loguniform(0.001, 0.1),
         "batch_size": tune.grid_search([300]),
         "num_epochs": max(1000000 // corpus_size, 1),
         "test_freq": max(10000 // corpus_size, 1),
@@ -47,9 +48,9 @@ if __name__ == "__main__":
         name=experiment_name,
         config=config,
         # num_samples=1 if args.smoke_test else 1000,
-        num_samples=1,
+        num_samples=100,
         # trial_name_creator=trial_str_creator,
-        resources_per_trial={"cpu": 6, "gpu": 1},
+        resources_per_trial={"cpu": 4, "gpu": 8},
         stop={"training_iteration": 1 if args.smoke_test else config["num_epochs"]},
         checkpoint_at_end=True,
         verbose=1,
